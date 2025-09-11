b3869526915e1d2ac010c444216d29272001c9f8
matteo poli <oxo@matteos-MacBook-Pro.local>
2025-09-11 21:18:39 +0200

    img si vedono

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-
+import { useGSAP } from '@gsap/react';
 
 import Router from './Router/Router';
 import Header from './components/Header';
 import SplashScreen from './components/SpashScreen';
 import ScrollToTop from '../src/components/ScrollToTop';
 
-// Import PortfolioGrid
-import PortfolioGrid from "./components/PortfolioGrid";
-
-// Registra solo i plugin GSAP validi
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, useGSAP);
 
 function App() {
   const [showSplash, setShowSplash] = useState(true);
@@ -30,11 +26,6 @@ function App() {
         <>
           <Header />
           <ScrollToTop />
-
-          {/* Qui inserisci PortfolioGrid */}
-          <PortfolioGrid />
-
-          {/* Router se vuoi ancora mantenere la navigazione */}
           <Router />
         </>
       )}
