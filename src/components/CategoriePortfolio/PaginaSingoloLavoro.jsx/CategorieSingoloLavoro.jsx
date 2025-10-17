26fc3bb3d4ceec7b747699144f1a3a92852c2919
matteo poli <oxo@matteos-MacBook-Pro.local>
2025-10-17 16:59:55 +0200

    andiamoo

--- src/components/CategoriePortfolio/PaginaSingoloLavoro.jsx/CategorieSingoloLavoro.jsx
+++ src/components/CategoriePortfolio/PaginaSingoloLavoro.jsx/CategorieSingoloLavoro.jsx
@@ -7,7 +7,7 @@ const Categorie = () => {
     <div className="flex flex-col md:flex-row justify-between items-center md:items-center pt-[130px] md:pt-[100px] lg:pt-[150px] px-4 md:px-10 lg:px-[80px] w-full gap-6 ">
 
       {/* Icone a sinistra */}
-      <div className="flex gap-6 z-[9999]">
+      <div className="flex gap-6 z-[50]">
         <Link to="/Portfolio" className="hover:text-white  transition">
           <FontAwesomeIcon icon={faThLarge} className="text-gray-600 text-2xl" />
         </Link>
@@ -17,7 +17,7 @@ const Categorie = () => {
       </div>
 
       {/* Categorie a destra */}
-      <div className="flex flex-wrap gap-4 md:gap-6 lg:gap-8 py-2 z-[9999]">
+      <div className="flex flex-wrap gap-4 md:gap-6 lg:gap-8 py-2 z-[50]">
         <Link to="/WebDesign" className="text-white hover:text-gray-600 hover:decoration-transparent transition text-[13px] md:text-2xl">
           Web Design
         </Link>
